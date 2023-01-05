1f72527fe2764d1d0441102580d17c0016925a9e
adrienanor <adrienanor14@gmail.com>
2023-01-05 23:43:52 +0100

    Update model Assignment

--- model/assignment.js
+++ model/assignment.js
@@ -5,7 +5,10 @@ let AssignmentSchema = Schema({
     id: Number,
     dateDeRendu: Date,
     nom: String,
-    rendu: Boolean
+    rendu: Boolean,
+    auteur: String,
+    note: Number,
+    remarque: String
 });
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
